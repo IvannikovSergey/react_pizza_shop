cc96dbf161264ae6f25a7fe6e5aa21a23125ebfd
Sergey <nagoymail@gmail.com>
2022-05-11 17:48:40 +0500

    создание компонента Popup menu

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,11 +1,17 @@
-import React from 'react';
+import React, {useState} from 'react';
 
-function Categories({items}) {
+function Categories({items, onClickItem}) {
+
+    const [activeItem, setActiveItem] = useState(null);
+
     return (
         <div className="categories">
             <ul>
-                <li className="active">Все</li>
-                {items.map(item => <li>{item}</li>)}
+                <li className={activeItem === null ? 'active' : ''} onClick={() => setActiveItem(null)}>Все</li>
+                {items && items.map((item, index) =>
+                    <li className={activeItem === index ? 'active' : ''}
+                        onClick={() => setActiveItem(index)}
+                        key={`${item}_${index}`}>{item}</li>)}
             </ul>
         </div>
     )
